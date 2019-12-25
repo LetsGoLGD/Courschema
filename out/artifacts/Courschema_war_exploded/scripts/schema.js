8a523e5ca1b03f8bff05610de1f6fefd60965f32
Iroha <944647289@qq.com>
2019-12-25 17:18:33 +0800

    latest version

--- out/artifacts/Courschema_war_exploded/scripts/schema.js
+++ out/artifacts/Courschema_war_exploded/scripts/schema.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
     $('#schemaTable').tablesorter()
 });
+$(function () {
+    $('#ss').click(function () {
+        var sstxt = $('#filtertxt').val();
+        $("table tbody tr").hide().filter(":contains('" + sstxt + "')").show();
+    })
+})
